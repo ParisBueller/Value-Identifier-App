f1eaebcc596de6b2ed571da22a89dff44e021821
Paris Ward <wardparis.pw@gmail.com>
2018-08-16 22:14:51 -0700

    connected firestore data stream to material table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs/Observable';
+import { AngularFirestore } from 'angularfire2/firestore';
 
-
+import { PlayerService } from './../services/player.service';
+import { Player } from './../models/Player';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +13,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  players: Player[];
+  displayedColumns = [ 'position', 'player','projection','salary','dpp'];
+  dataSource = new PlayerDataSource(this.player);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   
+
+  constructor(private player : PlayerService, private afs: AngularFirestore) {
+ 
+    
+  }
+
 }
+  export class PlayerDataSource extends DataSource<any> {
+    
+    constructor(private player: PlayerService) {
+      super()
+     
+    }
+    
+    connect() {
+      return this.player.getPlayers();
+      
+    }
+
+    disconnect() {
+
+    }
+  }
+
+  
+
--- src/app/players/players.component.ts
+++ src/app/players/players.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { DataSource } from '@angular/cdk/collections';
+import { Observable } from 'rxjs/Observable';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 import { PlayerService } from '../../services/player.service';
 import { Player } from '../../models/Player';
@@ -10,34 +13,35 @@ import { Player } from '../../models/Player';
   styleUrls: ['./players.component.css']
 })
 export class PlayersComponent implements OnInit {
-  players: Player[];
-  columnsToDisplay = [ 'position', 'player','projection','salary','dpp'];
-  dataSource: MatTableDataSource<Player>;
+  // players: Player[];
+  // displayedColumns = [ 'position', 'player','projection','salary','dpp'];
+  // dataSource;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+  // @ViewChild(MatPaginator) paginator: MatPaginator;
+  // @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private playerService : PlayerService) {
+  constructor(private players : PlayerService, private afs: AngularFirestore) {
 
-    this.dataSource = new MatTableDataSource(this.players);
+    //this.dataSource = new MatTableDataSource(this.players);
   }
 
   
 
-  ngOnInit() {
-    this.playerService.getPlayers().subscribe(players => {
-      this.players = players
-      console.log(players);
-    });
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+ ngOnInit() {
+    // this.playerService.getPlayers().subscribe(players => {
+    //   this.players = players
+    //   console.log(players);
+    // });
+  
+    // this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
   }
 
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  // applyFilter(filterValue: string) {
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
+  //   if (this.dataSource.paginator) {
+  //     this.dataSource.paginator.firstPage();
+  //   }
+  // }
 }
